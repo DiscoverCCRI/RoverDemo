1ef814099c1f8d7cad410a2980ee72400102c20b
Cody Beck <cjb873@nau.edu>
2022-09-08 11:06:59 -0700

    Update line_follower_demo.py

--- line_follower_demo.py
+++ line_follower_demo.py
@@ -10,7 +10,7 @@ def main():
     
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     roslaunch.configure_logging(uuid)
-    launch = roslaunch.parent.ROSLaunchParent(uuid, ["$(find RoverDemo)/line_follower.launch"])
+    launch = roslaunch.parent.ROSLaunchParent(uuid, ["line_follower.launch"])
     launch.start()
     rospy.loginfo("Launched")
     rospy.sleep(45)
