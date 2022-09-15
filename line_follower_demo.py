eb460f10487c6c53f2b3b12ec32e113f07785150
Cody Beck <cjb873@nau.edu>
2022-09-15 09:19:01 -0700

    Update line_follower_demo.py

--- line_follower_demo.py
+++ line_follower_demo.py
@@ -3,18 +3,19 @@
 
 import roslaunch
 import rospy
-
+from rover_api.discover_camera import Camera
 
 def main():
 
-    
+    cam = Camera()
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     roslaunch.configure_logging(uuid)
     launch = roslaunch.parent.ROSLaunchParent(uuid, ["line_follower.launch"])
     launch.start()
+    cam.start_recording()
     rospy.loginfo("Launched")
     rospy.sleep(45)
-    
+    cam.stop_recording()
 
     launch.shutdown()
 
