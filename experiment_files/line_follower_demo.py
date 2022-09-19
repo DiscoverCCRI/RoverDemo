a73c8fcee1c4189375f34b8bcda8aff8b70469da
Cody Beck <cjb873@nau.edu>
2022-09-19 10:18:32 -0700

    Removed camera function

--- experiment_files/line_follower_demo.py
+++ experiment_files/line_follower_demo.py
@@ -4,12 +4,10 @@
 import roslaunch
 import rospy
 from std_msgs.msg import Bool
-from rover_api.discover_camera import Camera
 
 
 def main():
 
-    cam = Camera()
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 
     pub = rospy.Publisher("/finished", Bool, queue_size=10)
@@ -20,13 +18,11 @@ def main():
     roslaunch.configure_logging(uuid)
     launch = roslaunch.parent.ROSLaunchParent(uuid, ["line_follower.launch"])
 
-    cam.start_recording()
     launch.start()
     rospy.loginfo("Launched")
     rospy.sleep(90)
 
     launch.shutdown()
-    cam.stop_recording()
 
     # publish finished message
     msg.data = True
