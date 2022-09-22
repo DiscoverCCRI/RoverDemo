06d097f4207f57346c799f7e02c4dc0545965ea4
Cody Beck <cjb873@nau.edu>
2022-09-22 12:32:57 -0700

    Change to not use camera

--- experiment_files/line_follower_demo.py
+++ experiment_files/line_follower_demo.py
@@ -4,11 +4,11 @@
 import roslaunch
 import rospy
 from std_msgs.msg import Bool
-from rover_api.discover_camera import Camera
+
 
 
 def main():
-    cam = Camera()
+    rospy.init_node("Line_Follower")
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 
     pub = rospy.Publisher("/finished", Bool, queue_size=10)
@@ -19,13 +19,11 @@ def main():
     roslaunch.configure_logging(uuid)
     launch = roslaunch.parent.ROSLaunchParent(uuid, ["line_follower.launch"])
 
-    cam.start_recording()
     launch.start()
     rospy.loginfo("Launched")
     rospy.sleep(90)
 
     launch.shutdown()
-    cam.stop_recording()
 
     # publish finished message
     msg.data = True
